a49b7bd89c59d960c7f9a2f69b92af1f8e53e8e9
Luke Jones <nnblc565@gmail.com>
2021-12-06 15:06:11 +1030

    Fixed positions of subTrees being on the corner instead of the centre.

--- Assets/QuadTree/QuadTree.cs
+++ Assets/QuadTree/QuadTree.cs
@@ -500,6 +500,8 @@ namespace QuadTree
                     offset.x *= targetTree % 2 == 0 ? -1 : 1;
                     //If targetTree is 2 - 3, bottom otherwise top
                     offset.y *= targetTree > 1 ? -1 : 1;
+                    //Put it in the centre instead of the corner
+                    offset *= 0.5f;
                     //Create a new tree
                     _subTrees[targetTree] = new QuadTree<T>(_depth - 1, _centre.x + offset.x, _centre.y + offset.y, _halfExtents.x / 2, _halfExtents.y / 2);
                 }
