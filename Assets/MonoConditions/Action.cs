bc9d86a305304f63665ec89274d0164a53ec0c68
Luke Jones <nnblc565@gmail.com>
2022-03-23 20:42:09 +1030

    Created a MonoCondition system because I tend to create theses systems a lot.

---
+++ Assets/MonoConditions/Action.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MonoConditions
+{
+    public abstract class Action<T> : ScriptableObject
+    {
+        /// <summary>
+        /// Called by MonoCondition when all conditions have passed.
+        /// </summary>
+        /// <param name="obj">The data on the mono</param>
+        /// <param name="object">The gameObject the mono was on</param>
+        public abstract void Execute(T obj, GameObject @object);
+    }
+}
---
+++ Assets/MonoConditions/Condition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MonoConditions
+{
+    public abstract class Condition<T> : ScriptableObject
+    {
+        /// <summary>
+        /// Checked as often as specified
+        /// </summary>
+        /// <param name="obj">Data from connected monoconditions</param>
+        /// <returns></returns>
+        public abstract bool IsTrue(T obj);
+    }
+}
