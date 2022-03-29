e617cb6de15726f62e7e9896dd17103bbbc513c0
Luke Jones <nnblc565@gmail.com>
2022-03-29 10:41:15 +1030

    More list & array extensions.

--- Assets/Extensions/Extensions.cs
+++ Assets/Extensions/Extensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 public static class Extensions
 {
+    private static Random r = null;
     /// <summary>
     /// Returns true if the array is null or empty
     /// </summary>
@@ -33,6 +34,48 @@ public static class Extensions
     public static bool IsNullOrEmpty<T1, T2>(this Dictionary<T1, T2> dic)
     {
         return dic == null || dic.Count == 0;
+    }
+    /// <summary>
+    /// Gets a random item from the array
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="a"></param>
+    /// <returns>Returns a random item from the array. Returns default if array is empty or null</returns>
+    public static T GetRandom<T>(this T[] a)
+    {
+        if (a.IsNullOrEmpty())
+            return default;
+
+        if (r == null)
+            r = new Random();
+        //r ??= new Random(); << Need to update c# to 8.0
+
+        return a[r.Next(a.Length)];
+    }
+    /// <summary>
+    /// Gets a random item from the list
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="l"></param>
+    /// <param name="remove">To remove the random item from the list or not</param>
+    /// <returns>Returns a random item from the list. Returns default if list is empty or null</returns>
+    public static T GetRandom<T>(this List<T> l, bool remove = false)
+    {
+        if (l.IsNullOrEmpty())
+            return default;
+
+        if (r == null)
+            r = new Random();
+        //r ??= new Random(); << Need to update c# to 8.0
+
+        if (!remove)
+            return l[r.Next(l.Count)];
+
+        int index = r.Next(l.Count);
+
+        T ret = l[index];
+        l.RemoveAt(index);
+        return ret;
     }
 
     #region IsEven
@@ -117,5 +160,48 @@ public static class Extensions
         target -= d;
         target = Math.Abs(target);
         return target < tolerance;
+    }
+}
+
+public static class UnityExtensions
+{
+    public static bool DoesOverlap(this UnityEngine.Vector2 thisOrigin, UnityEngine.Vector2 thisDirection, UnityEngine.Vector2 otherOrigin, UnityEngine.Vector2 otherDirection, out UnityEngine.Vector2 overlapPoint)
+    {
+        thisDirection.Normalize();
+        otherDirection.Normalize();
+        float a1, a2;
+        a1 = UnityEngine.Mathf.Atan2(thisDirection.y, thisDirection.x);
+        a2 = UnityEngine.Mathf.Atan2(otherDirection.y, otherDirection.x);
+
+        if (a1 >= Math.PI)
+            a1 -= (float)Math.PI;
+        if (a2 >= Math.PI)
+            a2 -= (float)Math.PI;
+        //Make sure lines are not parallel
+        if (a1.Equals(a2, 0.001f))
+        {
+            overlapPoint = UnityEngine.Vector2.zero;
+            return false;
+        }
+
+        a1 -= a2;
+        a1 = Math.Abs(a1);
+
+        overlapPoint = UnityEngine.Vector2.zero;
+        //Check lines are not perpendicular
+        if (a1.Equals((float)(Math.PI / 2), 0.001f))
+        {   //Are perpendicular
+            UnityEngine.Vector2 aToB = otherOrigin - thisOrigin;
+            //Use Dot to get distance
+            a1 = UnityEngine.Vector2.Dot(aToB, thisDirection);
+            overlapPoint = thisOrigin + thisDirection * a1;
+            return true;
+        }
+
+        //Check lines are not 1,0 or 0,1
+
+        float c1, c2;
+
+        throw new NotImplementedException("DoesOverlap not complete");
     }
 }
