fcd406cb8724729f0a9528cc868b722a756ac723
Luke Jones <nnblc565@gmail.com>
2022-11-14 20:22:06 +1030

    System extensions for non alloc get & set for bool compression

--- Assets/Extensions/SystemExtensions.cs
+++ Assets/Extensions/SystemExtensions.cs
@@ -168,4 +168,205 @@ public static class SystemExtensions
                 trueIndex++;
             }
     }
+    //Note: I should probably make long, int, short and byte variants of these functions to allow for non-byte array storage.
+    /// <summary>
+    /// Stores up to 8 bools in a single byte. (Pretty sure it doesn't allocate memory)
+    /// </summary>
+    /// <param name="bools"></param>
+    /// <returns></returns>
+    public static byte StoreBoolsInByte(params bool[] bools)
+    {
+        byte ret = 0;
+        for (int b = 0; b < bools.Length; b++)
+        {   //Can only store 8 bools
+            if (b >= 8)
+                return ret;
+            //Not enough space left
+            //Should give us an index starting from the first byte in bytes[i]
+            int temp = b;
+
+            int valueAsByte = bools[b] ? 0b_0000_0001 : 0b_0000_0000; // Can't be bothered manually casting.
+            //Shift across to correct bit
+            byte t2 = (byte)(valueAsByte << temp);
+            //Store
+            ret |= t2;
+        }
+        return ret;
+    }
+    /// <summary>
+    /// Reads the bits of a byte as a bool[] for compressed data storage. DOES NOT ALLOCATE MEMORY IF inBools ASSIGNED
+    /// </summary>
+    /// <param name="b"></param>
+    /// <param name="inBools">Assign if you want to avoid memory allocation. Will write to this array (output in outBools)</param>
+    /// <param name="outBools">Set to inBools if inBools is assigned, then filled</param>
+    public static void GetBools(this byte b, in bool[] inBools, out bool[] outBools)
+    {   //Setup out bool
+        if (inBools == null)
+            outBools = new bool[8];
+        else
+            outBools = inBools;
+
+        for (int i = 0; i < 8; i++)
+        {   //Out of space to assign to exit now.
+            if (i >= outBools.Length)
+                return;
+            //Copy it to a safe place so we can mess around with it
+            byte temp = b;
+            //Bit shift so the value we are looking for is in the 1's column
+            int t = temp >> i;
+            //Simplest way to check if the 1 bit is 1 or 0.
+            //Idk if this is any faster than manually bit shifting to flush
+            outBools[i] = t % 2 == 1;
+        }
+    }
+    /// <summary>
+    /// Stores up to 16 bools in a single short. (Pretty sure it doesn't allocate memory)
+    /// </summary>
+    /// <param name="bools"></param>
+    /// <returns></returns>
+    public static short StoreBoolsInShort(params bool[] bools)
+    {
+        short ret = 0;
+        for (int b = 0; b < bools.Length; b++)
+        {   //Can only store 8 bools
+            if (b >= 16)
+                return ret;
+            //Not enough space left
+            //Should give us an index starting from the first byte in bytes[i]
+            int temp = b;
+
+            int valueAsByte = bools[b] ? 0b_0000_0001 : 0b_0000_0000; // Can't be bothered manually casting.
+            //Shift across to correct bit
+            short t2 = (short)(valueAsByte << temp);
+            //Store
+            ret |= t2;
+        }
+        return ret;
+    }
+    /// <summary>
+    /// Reads the bits of a byte as a bool[] for compressed data storage. DOES NOT ALLOCATE MEMORY IF inBools ASSIGNED
+    /// </summary>
+    /// <param name="b"></param>
+    /// <param name="inBools">Assign if you want to avoid memory allocation. Will write to this array (output in outBools)</param>
+    /// <param name="outBools">Set to inBools if inBools is assigned, then filled</param>
+    public static void GetBools(this short b, in bool[] inBools, out bool[] outBools)
+    {   //Setup out bool
+        if (inBools == null)
+            outBools = new bool[16];
+        else
+            outBools = inBools;
+
+        for (int i = 0; i < 16; i++)
+        {   //Out of space to assign to exit now.
+            if (i >= outBools.Length)
+                return;
+            //Copy it to a safe place so we can mess around with it
+            short temp = b;
+            //Bit shift so the value we are looking for is in the 1's column
+            int t = temp >> i;
+            //Simplest way to check if the 1 bit is 1 or 0.
+            //Idk if this is any faster than manually bit shifting to flush
+            outBools[i] = t % 2 == 1;
+        }
+    }
+    /// <summary>
+    /// Stores up to 16 bools in a single short. (Pretty sure it doesn't allocate memory)
+    /// </summary>
+    /// <param name="bools"></param>
+    /// <returns></returns>
+    public static int StoreBoolsInInt(params bool[] bools)
+    {
+        int ret = 0;
+        for (int b = 0; b < bools.Length; b++)
+        {   //Can only store 8 bools
+            if (b >= 32)
+                return ret;
+            //Not enough space left
+            //Should give us an index starting from the first byte in bytes[i]
+            int temp = b;
+
+            int valueAsByte = bools[b] ? 0b_0000_0001 : 0b_0000_0000; // Can't be bothered manually casting.
+            //Shift across to correct bit
+            int t2 = (int)(valueAsByte << temp);
+            //Store
+            ret |= t2;
+        }
+        return ret;
+    }
+    /// <summary>
+    /// Reads the bits of a byte as a bool[] for compressed data storage. DOES NOT ALLOCATE MEMORY IF inBools ASSIGNED
+    /// </summary>
+    /// <param name="b"></param>
+    /// <param name="inBools">Assign if you want to avoid memory allocation. Will write to this array (output in outBools)</param>
+    /// <param name="outBools">Set to inBools if inBools is assigned, then filled</param>
+    public static void GetBools(this int b, in bool[] inBools, out bool[] outBools)
+    {   //Setup out bool
+        if (inBools == null)
+            outBools = new bool[32];
+        else
+            outBools = inBools;
+
+        for (int i = 0; i < 32; i++)
+        {   //Out of space to assign to exit now.
+            if (i >= outBools.Length)
+                return;
+            //Copy it to a safe place so we can mess around with it
+            int temp = b;
+            //Bit shift so the value we are looking for is in the 1's column
+            int t = temp >> i;
+            //Simplest way to check if the 1 bit is 1 or 0.
+            //Idk if this is any faster than manually bit shifting to flush
+            outBools[i] = t % 2 == 1;
+        }
+    }
+    /// <summary>
+    /// Stores up to 16 bools in a single short. (Pretty sure it doesn't allocate memory)
+    /// </summary>
+    /// <param name="bools"></param>
+    /// <returns></returns>
+    public static long StoreBoolsInLong(params bool[] bools)
+    {
+        long ret = 0;
+        for (int b = 0; b < bools.Length; b++)
+        {   //Can only store 8 bools
+            if (b >= 64)
+                return ret;
+            //Not enough space left
+            //Should give us an index starting from the first byte in bytes[i]
+            int temp = b;
+
+            long valueAsByte = bools[b] ? 0b_0000_0001 : 0b_0000_0000; // Can't be bothered manually casting.
+            //Shift across to correct bit
+            long t2 = (long)(valueAsByte << temp);
+            //Store
+            ret |= t2;
+        }
+        return ret;
+    }
+    /// <summary>
+    /// Reads the bits of a byte as a bool[] for compressed data storage. DOES NOT ALLOCATE MEMORY IF inBools ASSIGNED
+    /// </summary>
+    /// <param name="b"></param>
+    /// <param name="inBools">Assign if you want to avoid memory allocation. Will write to this array (output in outBools)</param>
+    /// <param name="outBools">Set to inBools if inBools is assigned, then filled</param>
+    public static void GetBools(this long b, in bool[] inBools, out bool[] outBools)
+    {   //Setup out bool
+        if (inBools == null)
+            outBools = new bool[64];
+        else
+            outBools = inBools;
+
+        for (int i = 0; i < 64; i++)
+        {   //Out of space to assign to exit now.
+            if (i >= outBools.Length)
+                return;
+            //Copy it to a safe place so we can mess around with it
+            long temp = b;
+            //Bit shift so the value we are looking for is in the 1's column
+            long t = temp >> i;
+            //Simplest way to check if the 1 bit is 1 or 0.
+            //Idk if this is any faster than manually bit shifting to flush
+            outBools[i] = t % 2 == 1;
+        }
+    }
 }
