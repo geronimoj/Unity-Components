395acf8203a05ee2c901607f964b2ca0afa3c7e9
Luke Jones <nnblc565@gmail.com>
2023-04-02 15:07:30 +0930

    Started Unity extensions.

---
+++ Assets/Extensions/UnityExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UnityExtensions
+{
+    public static void SafeSetActive(this GameObject obj, bool state)
+    {
+        if (obj)
+            obj.SetActive(state);
+    }
+}
